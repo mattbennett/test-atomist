45cb71fbb1eb7b4cdee57cb03ed1ea242f19b456
Matt Bennett <matt@bennett.name>
2019-04-15 20:35:27 +0100

    another url change

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ initializeDb( db => {
 	app.use(middleware({ config, db }));
 
     // hello
-    app.get('/hello', (req, res) => {
+    app.get('/hello-atomist', (req, res) => {
         res.json({ description });
     });
 
